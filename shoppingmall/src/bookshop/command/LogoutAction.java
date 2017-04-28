337330d3551a7a58a0d994a4688c8686d2d19a61
Daeseung <iamdaeseung@gmail.com>
2017-04-28 16:20:09 +0900

    o

---
+++ shoppingmall/src/bookshop/command/LogoutAction.java
@@ -0,0 +1,17 @@
+package bookshop.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bookshop.process.CommandAction;
+
+public class LogoutAction implements CommandAction{
+
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		// TODO Auto-generated method stub
+		
+		return "/member/logout.jsp";
+	}
+
+}
